ded16e8ae5f2190c4571726851d7f324634f4710
ArcD7 <pokemongochamp47@gmail.com>
2022-08-05 02:30:55 +0530

    Changed file handling during upload.

--- scheme/views.py
+++ scheme/views.py
@@ -3,49 +3,45 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib import messages
 from .forms import CsvModelForm
-from .models import ResourceIndex, Grants, Metadata
-import csv
-import time
+from .models import ResourceIndex, Grants, Metadata, FileManagement
 
-# Create your views here.
+
+def index(request):
+    
+    all_datasets =[]
+    get_all_data = ResourceIndex.objects.all()
+    for files in get_all_data:
+        name = files.name
+        all_datasets.append(name)
+    return render(request, "scheme/index.html", {"datasets": all_datasets})
+
+
 def upload_file_view(request):
-    #return HttpResponse('Upload File')
     #if request.method == 'POST':
     form = CsvModelForm(request.POST or None, request.FILES or None)
     if form.is_valid():
-        # file is saved
-        form.save()
+        name = form.cleaned_data['name']
+        description = form.cleaned_data['description']
+        tags = form.cleaned_data['tags']
+        metadata_file = form.cleaned_data['metadata_file']
+        
+        # Lets get the dataset files that were uploaded
+        files = request.FILES.getlist('file_name')
+        file_instance = ResourceIndex.objects.create(
+                file_name=name,
+                metadata_file=metadata_file,
+                name=name,
+                description=description,
+                tags=tags
+            )
+        print(file_instance, file_instance.resource_id)
+        
+        for f in files:
+            FileManagement.objects.create(
+                file_name=f, 
+                resource_id=file_instance
+            )
         form = CsvModelForm()
-        print(request.FILES)
-        name = "csv/" + request.FILES['file_name'].name
-        print(name)
-        file_obj = ResourceIndex.objects.filter(file_name=name)
-        print(file_obj)
-        with open(settings.MEDIA_ROOT+ "/" + name) as csv_file:
-            data = csv.reader(csv_file)
-            next(data)
-            for row in data:
-                Grants.objects.create(
-                budgetFor=row[0],
-                type=row[1],
-                fiscalYear=row[2],
-                indicators=row[3],
-                budgetType=row[4],
-                value=row[5],
-                resource_id=file_obj[0]
-                )
-        if request.FILES['metadata_file']:
-            meta_name = "metadata/" + request.FILES['metadata_file'].name
-            with open(settings.MEDIA_ROOT+ "/" + meta_name) as csv_file:
-                data = csv.reader(csv_file)
-                #next(data)
-                for row in data:
-                    Metadata.objects.create(
-                    heading=row[0],
-                    description=row[1],
-                    resource_id=file_obj[0]
-                    )
-                    
         messages.success(request, "Data Added Successfully")
     return render(
         request, 
