00026df700246fe9c086daf708deca7406a8b62d
ArcD7 <pokemongochamp47@gmail.com>
2022-08-06 03:45:42 +0530

    Urls changes for file download,

--- scheme/urls.py
+++ scheme/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import upload_file_view, index, dataset_home
+from .views import upload_file_view, index, dataset_home, dataset_download
 
 urlpatterns = [
     path('', index),
     path('upload/', upload_file_view, name='upload-view'),
     path("<str:name>", dataset_home, name='dataset-home'),
+    path("download/<str:name>", dataset_download, name='dataset-download')
 ]
