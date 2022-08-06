0e1bf8754747be6fdde8b8958eef0e420d02e8f5
ArcD7 <pokemongochamp47@gmail.com>
2022-08-06 19:56:39 +0530

    UI changes to display metadata.

--- scheme/urls.py
+++ scheme/urls.py
@@ -4,6 +4,6 @@ from .views import upload_file_view, index, dataset_home, dataset_download
 urlpatterns = [
     path('', index),
     path('upload/', upload_file_view, name='upload-view'),
-    path("<str:name>", dataset_home, name='dataset-home'),
+    path("dataset/<str:name>", dataset_home, name='dataset-home'),
     path("download/<str:name>", dataset_download, name='dataset-download')
 ]
