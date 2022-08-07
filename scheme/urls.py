6a952aa1d05bfb1c3ec0eb86246198943ffce4a1
ArcD7 <pokemongochamp47@gmail.com>
2022-08-07 14:38:22 +0530

    Add an upload and back button.

--- scheme/urls.py
+++ scheme/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import upload_file_view, index, dataset_home, dataset_download
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='homepage'),
     path('upload/', upload_file_view, name='upload-view'),
     path("dataset/<str:name>", dataset_home, name='dataset-home'),
     path("download/<str:name>", dataset_download, name='dataset-download')
